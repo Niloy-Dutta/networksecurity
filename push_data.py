5e9e097869e17446b4d0680251a4c1c17f4cbc29
Niloy Dutta Anik <niloydutta2525@gmail.com>
2026-01-28 16:21:14 +0600

    Data ingestion completed

--- push_data.py
+++ push_data.py
@@ -46,12 +46,4 @@ class NetworkDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-if __name__ == '__main__': 
-            FILE_PATH = 'Network_Data\phisingData.csv'
-            DATABASE = 'NILOYAI'
-            Collection = "NetworkData"
-            networkobj = NetworkDataExtract()
-            records = networkobj.csv_to_json_converter(file_path=FILE_PATH)
-            print(records)
-            no_of_records = networkobj.insert_data_mongodb(records=records,database=DATABASE,collection=Collection)
-            print(no_of_records) 
+ 
